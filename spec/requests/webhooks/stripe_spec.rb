acef4bbc8fab2d2eb6b940c4fd5caa23260b49e0
osmarluz <osmar.luz@gmail.com>
2023-12-19 16:32:20 -0300

    Fix Rubocop offenses

--- spec/requests/webhooks/stripe_spec.rb
+++ spec/requests/webhooks/stripe_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require '/app/spec/support/stripe_helper.rb'
+require '/app/spec/support/stripe_helper'
 
 describe 'POST /receive/stripe' do
   include StripeHelper
